48a72a9d8c73a534e3b6cc7e6cfaa02b53ef51cf
kinan Abbas <kinan.magnamedia@gmail.com>
2019-07-25 21:10:50 +0300

    Final Changes

---
+++ najjar.biz/Constants/Application.cs
@@ -0,0 +1,8 @@
+﻿namespace najjar.biz.Constants
+{
+    public class Application
+    {
+        public const string Name = "ASP.NET MVC Boilerplate";
+        public const string ShortName = "Boilerplate";
+    }
+}
---
+++ najjar.biz/Constants/ControllerName.cs
@@ -0,0 +1,8 @@
+﻿namespace najjar.biz.Constants
+{
+    public static class ControllerName
+    {
+        public const string Error = "Error";
+        public const string Home = "Home";
+    }
+}
