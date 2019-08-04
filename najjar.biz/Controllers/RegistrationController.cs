a33c7833f518eef897ccdd38c98d816a97a0f41f
kinan Abbas <kinan.magnamedia@gmail.com>
2019-08-04 21:11:58 +0300

    design

---
+++ najjar.biz/Controllers/RegistrationController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using najjar.biz.Models;
+using najjar.biz.Context;
+
+namespace najjar.biz.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private ApplicationDataContext db = new ApplicationDataContext();
+
+        // GET: /Registration/
+        public ActionResult Index()
+        {
+            var registrations = db.Registrations.Include(r => r.Employee).Include(r => r.Test);
+            return View(registrations.ToList());
+        }
+
+        // GET: /Registration/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // GET: /Registration/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "Name");
+            ViewBag.TestId = new SelectList(db.Tests, "Id", "Name");
+            return View();
+        }
+
+        // POST: /Registration/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,RegistrationDate,ExpiresDate,Token,TestId,EmployeeId")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Registrations.Add(registration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "Name", registration.EmployeeId);
+            ViewBag.TestId = new SelectList(db.Tests, "Id", "Name", registration.TestId);
+            return View(registration);
+        }
+
+        // GET: /Registration/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "Name", registration.EmployeeId);
+            ViewBag.TestId = new SelectList(db.Tests, "Id", "Name", registration.TestId);
+            return View(registration);
+        }
+
+        // POST: /Registration/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,RegistrationDate,ExpiresDate,Token,TestId,EmployeeId")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(registration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "Name", registration.EmployeeId);
+            ViewBag.TestId = new SelectList(db.Tests, "Id", "Name", registration.TestId);
+            return View(registration);
+        }
+
+        // GET: /Registration/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: /Registration/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Registration registration = db.Registrations.Find(id);
+            db.Registrations.Remove(registration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
