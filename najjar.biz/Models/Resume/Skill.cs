a9edbd089c6ddb1fad3abe575b1b80f3353f8bd3
kinan Abbas <kinan.magnamedia@gmail.com>
2019-08-20 14:44:06 +0300

    addinglangskillmodel

---
+++ najjar.biz/Models/Resume/Skill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace najjar.biz.Models
+{
+    public class Skill:BasicModel
+    {
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+              
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        public int EmployeeProspectId { get; set; }
+
+        public EmployeeProspect EmployeePros { get; set; }
+      
+    }
+}
